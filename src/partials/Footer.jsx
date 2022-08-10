8d1bb537ba5ac471657b722c12f5045384260504
Shaheer Sarfaraz <shaheergoodboy@gmail.com>
2022-08-10 12:30:11 +0500

    deploy

---
+++ src/partials/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Footer() {
+  return (
+    <footer>
+      <div className="max-w-6xl mx-auto mt-4 px-4 sm:px-6">
+
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
