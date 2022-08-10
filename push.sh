4b8ff6529eab43ac1ff29605ff2c6f5ba44891bf
Shaheer Sarfaraz <shaheergoodboy@gmail.com>
2022-08-10 12:32:45 +0500

    deploy

--- push.sh
+++ push.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/DaKheera47/signiify.git origin gh-pages
+git push origin gh-pages
 
 cd -
